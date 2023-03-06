daf0d2c83fe2295bc8cef366cfe110b55c9f4096
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-03-07 00:07:12 +0500

    Adding missions structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Rockets from './routes/Rockets';
 import MyProfile from './routes/MyProfile';
-import NotFound from './routes/NotFound';
+import Missions from './routes/Missions';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route index element={<Rockets />} />
         <Route path="rockets" element={<Rockets />} />
         <Route path="my-profile" element={<MyProfile />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="missions" element={<Missions />} />
       </Route>
     </Routes>
   );
