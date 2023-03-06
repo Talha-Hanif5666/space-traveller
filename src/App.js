5213edb5dc0120bbe6e740ae1fc2d6e123fce920
Javier <javidx@gmail.com>
2023-03-06 17:40:13 +0200

    Added navbar Added base routes Added react router and redux Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Nav } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
+import Rockets from './routes/Rockets'
+import MyProfile from './routes/MyProfile'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* <Route index element={<Rockets />} />
+        <Route path="my-profile" element={<MyProfile />} /> */}
+      </Route>
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/planet.png'
+import { Link } from 'react-router-dom';
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto" as='ul'>
+            <Nav.Item as="li"  className='mx-2'>
+              <Link to="/books">Rockets</Link>
+            </Nav.Item>
+            <Nav.Item as="li" className='mx-2'>
+              <Link to="/books">Missions</Link>
+            </Nav.Item>
+            <Nav.Item as="li" className='mx-2'>
+              <Link to="/books">My Profile</Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
