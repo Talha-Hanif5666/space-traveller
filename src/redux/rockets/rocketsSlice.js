eaf33921ae8e945613ab168abf41589e3eedf3b4
Javier <javidx@gmail.com>
2023-03-08 13:18:50 +0200

    Added: Rockets GET methods from API List element display for Rockets

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,6 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {};
+
+const rocketsURL = 'https://api.spacexdata.com/v4/rockets'
+
+export const getRockets = createAsyncThunk('rockets/get', async () =>{
+  console.log('test');
+  fetch(rocketsURL).then(res => res.json()).then(rocketsData => console.log(rocketsData))
+})
 
 const rocketsSlice = createSlice({
   name: 'rockets',
--- src/routes/Rockets.jsx
+++ src/routes/Rockets.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Badge, Button } from 'react-bootstrap';
+import planet from '../assets/planet.png'
+// import store from '../redux/store';
+// import { getRockets } from '../redux/rockets/rocketsSlice';
 
+
+// store.dispatch(getRockets())
 function Rockets() {
   return (
     <div>
-      <h1>Rockets</h1>
-      <h3>Under development</h3>
+      <ul className='list-group list-group-flush'>
+        <li className='list-group-item'>
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={planet} className="img-fluid rounded-start" alt="..."/>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text"><Badge bg="primary" pill>Available</Badge> This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  <Button variant='primary'>Reserve rocket</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className='list-group-item'>
+          <div className="card mb-3">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={planet} className="img-fluid rounded-start" alt="..."/>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text"><Badge bg="secondary" pill>Reserved</Badge> This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  <Button variant='secondary'>Cancel reservation</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
