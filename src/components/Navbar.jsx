3b93b9a201c91ff23a6348eb867ec9a1865af96a
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-03-09 21:42:49 +0500

    Adding My profile table

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ function BasicExample() {
             className="d-inline-block align-top"
             alt="React Bootstrap logo"
           />
+          <span style={{ fontWeight: 'bold', fontSize: '30px', marginLeft: '10px' }}>Space Traveler&apos;s Hub</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
--- src/routes/MyProfile.jsx
+++ src/routes/MyProfile.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { useSelector } from 'react-redux';
 
-function MyProfile() {
+const MyProfile = () => {
+  const mission = useSelector((state) => state.missions);
+  const display = mission.missions.filter((mission) => mission.reserved === true);
+
   return (
-    <div>
-      <h1>My profile</h1>
-      <h3>Under development</h3>
-    </div>
+    <main id="profileSpace">
+      <aside className="profmission">
+        <h2>My Missions</h2>
+        <Table bordered hover>
+          <tbody>
+            {display.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </aside>
+    </main>
   );
-}
+};
 
 export default MyProfile;
