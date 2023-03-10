7b20d8abe8c0c6830c25934d891023fe9d558e0e
Javier <javidx@gmail.com>
2023-03-10 16:06:37 +0200

    Updated basename for app

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/space-traveller">
       <Provider store={store}>
         <App />
       </Provider>
